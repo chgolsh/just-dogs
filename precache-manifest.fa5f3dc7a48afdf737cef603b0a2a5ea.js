7b538a71cea6214248c6e9cb0ae4da74720468ef
Chingiz Golyshev <chgolsh@yandex.ru>
2019-11-08 13:13:38 +0700

    Updates

---
+++ precache-manifest.fa5f3dc7a48afdf737cef603b0a2a5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee8c6395fed0f8e78300189d28e9128",
+    "url": "/just-dogs/index.html"
+  },
+  {
+    "revision": "f102317deec6570e6776",
+    "url": "/just-dogs/static/css/main.5e43d90b.chunk.css"
+  },
+  {
+    "revision": "dcf8a43d7d4708029443",
+    "url": "/just-dogs/static/js/2.48f3e69e.chunk.js"
+  },
+  {
+    "revision": "f102317deec6570e6776",
+    "url": "/just-dogs/static/js/main.0eea7954.chunk.js"
+  },
+  {
+    "revision": "3f13a1757b95546141bb",
+    "url": "/just-dogs/static/js/runtime-main.6d7da4d2.js"
+  }
+]);
